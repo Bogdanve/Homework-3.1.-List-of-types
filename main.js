626027a4e575e9f8e94a97e5eee61d4e974b7f06
Bogdanve <m1ravbogd4@gmail.com>
2024-08-07 22:24:19 +0300

    All types

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log(typeof false); //Boolean
+console.log(typeof 10); // Number
+console.log(typeof "st"); // String
+console.log(typeof Burger); // Undefined
+console.log(typeof 10n); // BigInt
+console.log(typeof Symbol); // Symbol
+console.log(typeof []); // Object
